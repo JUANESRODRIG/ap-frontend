1a2412b9cb792ebaa5a74c215098f91e403915b1
JUANESRODRIG <j.rodriguez@ofiservices.com>
2026-03-13 07:18:02 -0500

    Update invoiceService.ts

--- AP-Fronten_New/src/services/invoiceService.ts
+++ AP-Fronten_New/src/services/invoiceService.ts
@@ -19,7 +19,7 @@ export async function uploadInvoices(
     const response = await fetch('https://n8n.sofiatechnology.ai/webhook/invoices', {
         method: 'POST',
         body: formData,
-        // https://n8n.sofiatechnology.ai/webhook-test/upload-invoices
+
     });
 
     if (!response.ok) {
